cea6839c6d1af7b5217954268c6c81d91e1f1349
Isaac Cavalcante <48898725+isaaccavalcantef@users.noreply.github.com>
2021-10-09 22:21:48 -0300

    App finalizado

---
+++ Public/processa_envio.php
@@ -0,0 +1,12 @@
+    
+ <?php   
+    
+    require "../Htdoc/app-sendEmail/lib/PHPMailer/Exception.php";
+	require "../Htdoc/app-sendEmail/lib/PHPMailer/OAuth.php";
+	require "../Htdoc/app-sendEmail/lib/PHPMailer/PHPMailer.php";
+	require "../Htdoc/app-sendEmail/lib/PHPMailer/POP3.php";
+	require "../Htdoc/app-sendEmail/lib/PHPMailer/SMTP.php";
+
+	require "../Htdoc/app-sendEmail/processa_envio.php";
+
+    ?>
